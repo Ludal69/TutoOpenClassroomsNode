3f98c2ea212baebee167c36de5b59fa3a95942fe
DESKTOP-G4I7JNL\Ludo <lulu.713lg@gmail.com>
2024-06-16 14:49:50 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const stuffRoutes = require("./routes/stuff");
+const app = express();
+
+mongoose
+  .connect(
+    "mongodb+srv://Ludal69:ENm0B6pcn0gUTvNe@cluster0.uknrrov.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+module.exports = app;
